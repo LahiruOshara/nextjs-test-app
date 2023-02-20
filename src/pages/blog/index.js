21e339ddf0c6dd2a7d0d5ae250a24284d1418030
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2023-02-20 22:19:58 +0530

    nested routes

---
+++ src/pages/blog/index.js
@@ -0,0 +1,7 @@
+export default function Root(){
+    return(
+        <h1>
+            Blog Pgae
+        </h1>
+    )
+}
---
+++ src/pages/blog/second.js
@@ -0,0 +1,7 @@
+export default function Root(){
+    return(
+        <h1>
+            Second Blog Pgae
+        </h1>
+    )
+}
