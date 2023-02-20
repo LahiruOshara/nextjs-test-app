408f6c15f8cd2c065098a944e3d932750f55015e
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2023-02-20 22:56:16 +0530

    progmatic routing

--- src/pages/product/[productId]/index.js
+++ src/pages/product/[productId]/index.js
@@ -4,9 +4,15 @@ export default function ProductDetails() {
     const router = useRouter();
     const productId = router.query.productId;
 
+    const handleClick = () => {
+        console.log("Place order")
+        router.push('/')
+    }
+
     return (
         <dev>        
             <h1> Product {productId} Details Pgae </h1>
+            <button onClick={handleClick} >Add Product</button>
         </dev>
 
     )
