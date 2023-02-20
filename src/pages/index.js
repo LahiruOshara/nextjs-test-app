d4a571cff06c73f62196d28ce97f789867a74ac0
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2023-02-20 22:49:54 +0530

    'next/link'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
-export default function Root(){
-    return(
-        <h1>
-            Welcome... root
-        </h1>
+import Link from 'next/link'
+
+export default function Root() {
+    return (
+        <div>
+            <h1>
+                Welcome... root
+            </h1>
+            <Link href = { '/blog' } >
+                Blog
+            </Link>
+        </div>
     )
 }
